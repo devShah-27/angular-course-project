766e54fb66510ca2a0dbff8ff45f2f65d103c2c0
Dev Shah <shahdev149@gmail.com>
2024-02-06 18:13:27 +0530

    implement firebase connectivity using http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeEditComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule
+    BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,16 +10,23 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Pasta', 'Italian Pasta', 'https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
-      new Ingredient('Pasta', 3), new Ingredient('Marinara Sauce', 2)
-    ]),
-    new Recipe('Quesadilla', 'French Roti', 'https://images.unsplash.com/photo-1618040996337-56904b7850b9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
-      new Ingredient('Tortilla Chip', 1), new Ingredient('Beans', 30)
-    ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Pasta', 'Italian Pasta', 'https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
+  //     new Ingredient('Pasta', 3), new Ingredient('Marinara Sauce', 2)
+  //   ]),
+  //   new Recipe('Quesadilla', 'French Roti', 'https://images.unsplash.com/photo-1618040996337-56904b7850b9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
+  //     new Ingredient('Tortilla Chip', 1), new Ingredient('Beans', 30)
+  //   ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
